36850e02c1c148f7c1568e7ee383aab597ea3595
EFFETTO <100048888+effettot@users.noreply.github.com>
2023-04-10 11:49:18 +0200

    Create websocket.exs

---
+++ websocket.exs
@@ -0,0 +1,55 @@
+defmodule ChatServer do
+  require Logger
+
+  def start do
+    Logger.info("Starting ChatServer...")
+    {:ok, _} = Plug.Adapters.Cowboy.http(__MODULE__, [])
+  end
+
+  def init(_) do
+    Phoenix.Endpoint.CowboyWebSocket.init(
+      dispatcher: __MODULE__,
+      protocols: [:websocket],
+      handler: __MODULE__
+    )
+  end
+
+  def handle_in("join", _params, socket) do
+    Logger.info("User joined chat")
+    {:ok, assign(socket, :joined, true)}
+  end
+
+  def handle_in("message", %{"text" => message}, socket) do
+    Logger.info("Received message: #{message}")
+    broadcast(socket, "message", %{text: message})
+    {:noreply, socket}
+  end
+
+  def handle_out("message", payload, socket) do
+    push(socket, "message", payload)
+    {:ok, socket}
+  end
+
+  def handle_cast(_msg, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
+  defp broadcast(socket, event, payload) do
+    Enum.each(get_clients(socket), fn client ->
+      push(client, event, payload)
+    end)
+  end
+
+  defp push(socket, event, payload) do
+    Phoenix.Socket.broadcast!(socket, event, payload)
+  end
+
+  defp get_clients(socket) do
+    Phoenix.Channel.list(socket)
+    |> Enum.filter(&(&1 != socket))
+  end
+end
